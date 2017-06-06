98345d3275beebbbd3e4ea598abd3fe4fa50504f
tokelau <lobckina.nastya@yandex.ru>
2017-06-07 02:16:13 +0800

    Update main.cpp

--- circuit/main.cpp
+++ circuit/main.cpp
@@ -5,14 +5,18 @@
 
 using namespace std;
 
-int main() {
-	File arr;
-	if (!readFile("res-2.txt", &arr)) {
+int main(int argc, char** argv) {
+	if (argc < 3) { //если на вход подано слишком мало аргументов - сообщаем об этом
+		cout << "Not enough arguments" << endl;
+		return -1;
+	}
+	File arr; //создаем структуру 
+	if (!readFile(argv[1], &arr)) { //выполняем чтение файла
 		cout << "I can't read file" << endl;
 		return -1;
 	}
-	findCircuit(&arr, 0, 0);
-	if (!writeFile("out.txt", &arr)) {
+	findCircuit(&arr, 0, 0); //ищем контуры
+	if (!writeFile(argv[2], &arr)) { //записываем выходные данные
 		cout << "I can't write file" << endl;
 		return -1;
 	}
