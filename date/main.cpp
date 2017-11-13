ca7a888ee97cc5e05d0bcc97a504b896d3269e11
tokelau <lobckina.nastya@yandex.ru>
2017-11-13 21:46:20 +0800

    Update main.cpp

--- date/main.cpp
+++ date/main.cpp
@@ -4,10 +4,27 @@
 using namespace std;
 
 int main(void){
-	Date data("10.11.2017");
-	Date d1("04.05.16");
-	data = d1 + 21;
-	cout<<data;
+	Date date1;
+	cout << date1 << "(" << date1.dayWeek() <<")" << endl;
+	date1 += 5;
+	cout << "date1 + 5 days = " << date1 << endl;
+	date1 = date1 - 5;
+	cout << "date1 + 5 days - 5 days = " << date1 << endl;
+	cout << endl;
+
+	Date date2("13.11.17");
+	cout << date2 << "(" << date2.dayWeek() << ")" << endl;
+	date2 += 5;
+	cout << "date2 + 5 days = " << date2 << endl;
+	date2 = date2 - 5;
+	cout << "date2 + 5 days - 5 days = " << date2 << endl;
+	cout << endl;
+
+	Date date3("1.12.2017");
+	cout << date3 << "(" << date3.dayWeek() << ")"<< endl;
+	cout << "How many days between 13.11.2017 and 1.12.2017? " << date3.dist(date2) << endl;
+	cout << endl;
+	
 	system("pause");
 	return 1;
 }
